76fda7e3dee3b04955c74c0a69a8db40238510f3
Blaine Bush <blaine.bush@gmail.com>
2022-04-11 10:39:53 +0200

    Add base class info

--- character.go
+++ character.go
@@ -2,87 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
+	"time"
 )
 
-type Class struct {
-	id     int
-	name   string
-	baseXp int
-	levels map[int]struct {
-		xp int
-		hd struct {
-			base  int
-			bonus int
-		}
-		attackValue int
-		savingThrow int
-		slotCount   int
-		groupCount  int
-		raiseCount  int
-	}
-}
-
-type Group struct {
-	name        string
-	categoryId  int
-	attributeId int
-}
-
-type GroupCategory struct {
-	id int
-	name string
-}
-
-type Attribute struct {
-	id    int
-	name  string
-	score int
-}
-
-// GetAbbreviation returns the first three letters of an attribute in upper case.
-func (attr Attribute) GetAbbreviation() string {
-	return strings.ToUpper(attr.name[0:3])
-}
-
-type Character struct {
-	name       string
-	xp         int
-	classId    int
-	groups     []Group
-	attributes [6]Attribute
-}
-
-var (
-	classes = map[int]Class{
-		1: {name: "Deft", baseXp: 1500},
-		2: {name: "Strong", baseXp: 2000},
-		3: {name: "Wise", baseXp: 2500},
+func randomClass() Class {
+	classNames := [3]string{
+		"deft",
+		"strong",
+		"wise",
 	}
 
-	groupCategories = map[int]GroupCategory{
-		1:
-	}
-
-	attributes = map[int]Attribute{
-		1: {name: "Strength"},
-		2: {name: "Dexterity"},
-		3: {name: "Constitution"},
-		4: {name: "Intelligence"},
-		5: {name: "Wisdom"},
-		6: {name: "Charisma"},
-	}
-)
+	return classes[classNames[rand.Intn(len(classNames))]]
+}
 
 func main() {
-	class := classes[1]
-	fmt.Println("Name    :", class.name)
-	fmt.Println("Base XP :", class.baseXp)
+	// Seed the randomizer.
+	rand.Seed(time.Now().UnixNano())
+	// Randomly select a class.
+	class := randomClass()
+	// Randomly select a level.
+	level := rand.Intn(10) + 1
+	levelInfo := class.levels[level]
 
-	for index, val := range attributes {
-		fmt.Println("ID:          ", index)
-		fmt.Println("Attribute:   ", val.name)
-		fmt.Println("Abbreviation:", val.GetAbbreviation())
-		fmt.Println("")
-	}
+	fmt.Println("Class:", class.name)
+	fmt.Println("Level:", level)
+	fmt.Println(levelInfo)
 }
---
+++ vars.go
@@ -0,0 +1,376 @@
+package main
+
+type Class struct {
+	name   string
+	levels map[int]Level
+}
+
+type Level struct {
+	xp             int
+	hitDie         int
+	bonusHitPoints int
+	attackValue    int
+	savingThrow    int
+	slotCount      int
+	groupCount     int
+	raiseCount     int
+}
+
+type Group struct {
+	name      string
+	category  string
+	attribute string
+}
+
+type GroupCategory struct {
+	name           string
+	attributeCount int
+}
+
+type Attribute struct {
+	name         string
+	abbreviation string
+}
+
+type Character struct {
+	name       string
+	xp         int
+	classId    int
+	groups     []Group
+	attributes [6]Attribute
+}
+
+var (
+	attributes = map[string]Attribute{
+		"str": {name: "Strength", abbreviation: "STR"},
+		"dex": {name: "Dexterity", abbreviation: "DEX"},
+		"con": {name: "Constitution", abbreviation: "CON"},
+		"int": {name: "Intelligence", abbreviation: "INT"},
+		"wis": {name: "Wisdom", abbreviation: "WIS"},
+		"cha": {name: "Charisma", abbreviation: "CHA"},
+	}
+
+	groupCategories = map[string]GroupCategory{
+		"vocation":    {name: "Vocation", attributeCount: 1},
+		"affiliation": {name: "Affiliation", attributeCount: 1},
+		"species":     {name: "Species", attributeCount: 2},
+	}
+
+	classes = map[string]Class{
+		"deft": {
+			name: "Deft",
+			levels: map[int]Level{
+				1: {
+					xp:             0,
+					hitDie:         1,
+					bonusHitPoints: 0,
+					attackValue:    10,
+					savingThrow:    7,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     0,
+				},
+				2: {
+					xp:             1500,
+					hitDie:         2,
+					bonusHitPoints: 0,
+					attackValue:    11,
+					savingThrow:    8,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     1,
+				},
+				3: {
+					xp:             3000,
+					hitDie:         2,
+					bonusHitPoints: 1,
+					attackValue:    11,
+					savingThrow:    9,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     1,
+				},
+				4: {
+					xp:             6000,
+					hitDie:         3,
+					bonusHitPoints: 0,
+					attackValue:    12,
+					savingThrow:    10,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     2,
+				},
+				5: {
+					xp:             12000,
+					hitDie:         3,
+					bonusHitPoints: 1,
+					attackValue:    12,
+					savingThrow:    11,
+					slotCount:      2,
+					groupCount:     4,
+					raiseCount:     2,
+				},
+				6: {
+					xp:             24000,
+					hitDie:         4,
+					bonusHitPoints: 0,
+					attackValue:    13,
+					savingThrow:    12,
+					slotCount:      2,
+					groupCount:     4,
+					raiseCount:     3,
+				},
+				7: {
+					xp:             48000,
+					hitDie:         4,
+					bonusHitPoints: 1,
+					attackValue:    13,
+					savingThrow:    13,
+					slotCount:      3,
+					groupCount:     5,
+					raiseCount:     3,
+				},
+				8: {
+					xp:             96000,
+					hitDie:         5,
+					bonusHitPoints: 0,
+					attackValue:    14,
+					savingThrow:    14,
+					slotCount:      3,
+					groupCount:     5,
+					raiseCount:     4,
+				},
+				9: {
+					xp:             192000,
+					hitDie:         5,
+					bonusHitPoints: 1,
+					attackValue:    14,
+					savingThrow:    15,
+					slotCount:      3,
+					groupCount:     6,
+					raiseCount:     4,
+				},
+				10: {
+					xp:             384000,
+					hitDie:         6,
+					bonusHitPoints: 0,
+					attackValue:    15,
+					savingThrow:    16,
+					slotCount:      4,
+					groupCount:     6,
+					raiseCount:     5,
+				},
+			},
+		},
+		"strong": {
+			name: "Strong",
+			levels: map[int]Level{
+				1: {
+					xp:             0,
+					hitDie:         1,
+					bonusHitPoints: 2,
+					attackValue:    11,
+					savingThrow:    5,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     0,
+				},
+				2: {
+					xp:             2000,
+					hitDie:         2,
+					bonusHitPoints: 0,
+					attackValue:    11,
+					savingThrow:    6,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     1,
+				},
+				3: {
+					xp:             4000,
+					hitDie:         3,
+					bonusHitPoints: 0,
+					attackValue:    12,
+					savingThrow:    7,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     1,
+				},
+				4: {
+					xp:             8000,
+					hitDie:         4,
+					bonusHitPoints: 0,
+					attackValue:    13,
+					savingThrow:    8,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     2,
+				},
+				5: {
+					xp:             16000,
+					hitDie:         5,
+					bonusHitPoints: 0,
+					attackValue:    13,
+					savingThrow:    9,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     2,
+				},
+				6: {
+					xp:             32000,
+					hitDie:         6,
+					bonusHitPoints: 0,
+					attackValue:    14,
+					savingThrow:    10,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     3,
+				},
+				7: {
+					xp:             64000,
+					hitDie:         7,
+					bonusHitPoints: 0,
+					attackValue:    15,
+					savingThrow:    11,
+					slotCount:      3,
+					groupCount:     4,
+					raiseCount:     3,
+				},
+				8: {
+					xp:             128000,
+					hitDie:         8,
+					bonusHitPoints: 0,
+					attackValue:    15,
+					savingThrow:    12,
+					slotCount:      3,
+					groupCount:     4,
+					raiseCount:     4,
+				},
+				9: {
+					xp:             256000,
+					hitDie:         9,
+					bonusHitPoints: 0,
+					attackValue:    16,
+					savingThrow:    13,
+					slotCount:      3,
+					groupCount:     4,
+					raiseCount:     4,
+				},
+				10: {
+					xp:             512000,
+					hitDie:         10,
+					bonusHitPoints: 0,
+					attackValue:    17,
+					savingThrow:    14,
+					slotCount:      4,
+					groupCount:     5,
+					raiseCount:     5,
+				},
+			},
+		},
+		"wise": {
+			name: "Wise",
+			levels: map[int]Level{
+				1: {
+					xp:             0,
+					hitDie:         1,
+					bonusHitPoints: 1,
+					attackValue:    10,
+					savingThrow:    6,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     0,
+				},
+				2: {
+					xp:             2500,
+					hitDie:         2,
+					bonusHitPoints: 0,
+					attackValue:    11,
+					savingThrow:    7,
+					slotCount:      1,
+					groupCount:     2,
+					raiseCount:     1,
+				},
+				3: {
+					xp:             5000,
+					hitDie:         2,
+					bonusHitPoints: 1,
+					attackValue:    11,
+					savingThrow:    8,
+					slotCount:      2,
+					groupCount:     2,
+					raiseCount:     1,
+				},
+				4: {
+					xp:             10000,
+					hitDie:         3,
+					bonusHitPoints: 0,
+					attackValue:    11,
+					savingThrow:    9,
+					slotCount:      2,
+					groupCount:     3,
+					raiseCount:     2,
+				},
+				5: {
+					xp:             20000,
+					hitDie:         4,
+					bonusHitPoints: 0,
+					attackValue:    12,
+					savingThrow:    10,
+					slotCount:      3,
+					groupCount:     3,
+					raiseCount:     2,
+				},
+				6: {
+					xp:             40000,
+					hitDie:         4,
+					bonusHitPoints: 1,
+					attackValue:    12,
+					savingThrow:    11,
+					slotCount:      3,
+					groupCount:     3,
+					raiseCount:     3,
+				},
+				7: {
+					xp:             80000,
+					hitDie:         5,
+					bonusHitPoints: 0,
+					attackValue:    12,
+					savingThrow:    12,
+					slotCount:      4,
+					groupCount:     4,
+					raiseCount:     3,
+				},
+				8: {
+					xp:             160000,
+					hitDie:         6,
+					bonusHitPoints: 0,
+					attackValue:    13,
+					savingThrow:    13,
+					slotCount:      4,
+					groupCount:     4,
+					raiseCount:     4,
+				},
+				9: {
+					xp:             320000,
+					hitDie:         6,
+					bonusHitPoints: 1,
+					attackValue:    13,
+					savingThrow:    14,
+					slotCount:      5,
+					groupCount:     4,
+					raiseCount:     4,
+				},
+				10: {
+					xp:             640000,
+					hitDie:         7,
+					bonusHitPoints: 0,
+					attackValue:    13,
+					savingThrow:    15,
+					slotCount:      5,
+					groupCount:     5,
+					raiseCount:     5,
+				},
+			},
+		},
+	}
+)
